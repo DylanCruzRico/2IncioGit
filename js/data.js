0312d1fca5fd62289ac6543d8a07c1ecd1fd25e6
jughead cruz rico <dl.cruzrico@ugot.mx>
2023-02-09 10:18:36 -0600

    Ya acabamos

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'Por el resto',
+        path: '../music/Song 1.mp3',
+        artist: 'Christian Nodal',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'Vivo en el 6',
+        path: '../music/Song 2.mp3',
+        artist: 'Christian Nodal',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'Aqui abajo',
+        path: '../music/Song 3.mp3',
+        artist: 'Christian Nodal',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Ya no somos  ',
+        path: '../music/Song 4.mp3',
+        artist: 'Christian Nodal',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'Limon con Sal',
+        path: '../music/Song 5.mp3',
+        artist: 'Christian Nodal',
+        cover: '../images/cover 5.png'
+    }
+]
